5db12cc33277804d36951c42a41f993b48503351
bliu123 <bliu314159@gmail.com>
2015-09-13 16:02:14 -0700

    Update MAIN.cpp

--- MAIN.cpp
+++ MAIN.cpp
@@ -1,38 +1,21 @@
+#include <ctime> //Allow for more random numbers
+#include <cstdlib> //Allow for more random numbers
 #include <iostream>
-#include "Quadcopter.h"
 using namespace std;
 
-int8_t getPitch(){
-  int pitch1
-  cout << "Enter initial pitch (integer -127 to 127):\n"
-  cin >> pitch1
-  if (pitch1 < -127 || pitch1 > 127)
-  {
-    pitch1 = -123987
-  }
-    return pitch1
+int randPitch (int minPitch, int maxPitch)
+{
+  return rand() % (maxPitch - minPitch + 1) + minPitch
 }
 
-int8_t getRoll(){
-  int roll1
-  cout << "Enter initial roll (integer -127 to 127):\n"
-  cin >> roll1
-  if (roll1 < -127 || roll1 > 127)
-  {
-    roll1 = -123987
-  }
-    return pitch1
+}
+
+int getRoll(){
+
 }
 
 uint8_t getHeight(){
-  int height1
-  cout << "Enter desired height (integer 1-255):\n"
-  cin >> height1
-  if (height1 < 1 || height1 > 255)
-  {
-    height1 = -123987
-  }
-  return height1
+
 }
 
 int main(){
