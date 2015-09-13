aad3ee64e91603ad7e9921503a888abea37ffdab
bliu123 <bliu314159@gmail.com>
2015-09-13 15:09:23 -0700

    Update MAIN.cpp

--- MAIN.cpp
+++ MAIN.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
-#include <std
 #include "Quadcopter.h"
+
 
 int main(){
 int roll = 0;
 int pitch = 0;
 int height = 100;
 
-}
-
-void setup()
-{
-}
-
-void MainLoop()
-{
-  while(1){
-  
-  }
-}
 
